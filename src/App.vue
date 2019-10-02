69c58e62ba3bcb6e06a1a5550b7074b0ece71246
AndreaMyshkn <a.trkvsk@hotmail.com>
2019-10-02 00:37:57 -0500

    he

---
+++ src/App.vue
@@ -0,0 +1,24 @@
+<template lang="pug">
+  #app
+    p(v-show="showValue") {{value}}
+</template>
+
+<script>
+export default {
+  name: 'app',
+  
+  data () {
+    return {
+      msg: 'UVA', 
+      showValue: false, 
+      value: 'Algo'
+      
+    }
+  }
+}
+</script>
+
+<style lang="scss">
+
+@import './assets/scss/main.scss'
+</style>
